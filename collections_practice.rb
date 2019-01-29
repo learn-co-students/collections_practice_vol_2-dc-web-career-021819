47a2fad5f8348ae7b7c10ba9255b9289061a4fcd
Ammar Ali <funnyguy118@gmail.com>
2019-01-29 16:12:56 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
-# your code goes here
+def begins_with_r (array)
+  array.each {|item|
+    if item.chars.first != "r"
+      return FALSE
+    end
+  }
+  TRUE
+end
+
+def contain_a (array)
+    new_array = []
+  array.each {|item|
+    if item.chars.include? ("a")
+      new_array << item
+    end
+  }
+  new_array
+end
+
+def first_wa (array)
+  array.find {|item| item.slice(0..1) == "wa" }
+end
+
+def remove_non_strings (array)
+  array.each {|item|
+    if item.is_a? String
+    else
+        array.delete(item)
+    
+  }
+end
+
+def count_elements (array)
+  
+end
+
+def merge_data(array)
+  
+end
+
+def find_cool(array)
+  
+end
+
+def organize_schools(array)
+  
+end 
