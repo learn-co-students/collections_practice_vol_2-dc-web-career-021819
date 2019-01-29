4c4127c569d247660276f1924ab3c257422be258
Ammar Ali <funnyguy118@gmail.com>
2019-01-29 18:06:15 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,26 +22,55 @@ def first_wa (array)
 end
 
 def remove_non_strings (array)
+  new_array = []
   array.each {|item|
-    if item.is_a? String
-    else
-        array.delete(item)
-    
+    new_array << item if item.is_a? (String)
   }
+  new_array
 end
 
-def count_elements (array)
-  
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
 end
 
-def merge_data(array)
-  
+def merge_data(arr1, arr2)
+  arr2[0].map do |name, prop_hash|
+    new_prop_hash = {}
+    arr1.each do |new_attr_hash|
+      if new_attr_hash[:first_name] == name
+        new_prop_hash = prop_hash.merge(new_attr_hash)
+      end
+    end
+    new_prop_hash
+  end
 end
 
 def find_cool(array)
-  
+  container = []
+  array.each do |element|
+    container << element if element[:temperature] == "cool"
+  end
+  container
 end
 
-def organize_schools(array)
-  
-end 
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
